533576057b8648e7446d12640025d4a54f1ba82b
YR1017fr <a27536625@gmail.com>
2021-11-23 22:28:16 +0800

    env

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const register =require('./controllders/register');
 const signin =require('./controllders/signin');
 const scheme = require('./controllders/scheme');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db=knex({
     client:'pg',
     connection:{
         connectionString:process.env.DATABASE_URL,
-        ssl:true,
+        ssl:{
+            rejectUnauthorized: false
+        }
     }
 });
 
