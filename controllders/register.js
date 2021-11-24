df9deec21d466017430ec8c76ce8d24d49e6c23e
YR1017fr <a27536625@gmail.com>
2021-11-24 21:50:22 +0800

    spell wrong

--- controllders/register.js
+++ controllders/register.js
@@ -22,7 +22,7 @@ const handleRegister = (req,res,bcrypt,db) =>{
     if(data.email){
         res.json(data);
     }else{
-        res.statue(400).json('repeat email');
+        res.status(400).json('repeat email');
     }
 
 
