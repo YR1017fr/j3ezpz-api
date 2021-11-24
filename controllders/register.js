2568ff6db1212b83387fd993be20be4a4b71212e
YR1017fr <a27536625@gmail.com>
2021-11-24 23:30:57 +0800

    fix

--- controllders/register.js
+++ controllders/register.js
@@ -1,29 +1,31 @@
 const handleRegister = async function(req,res,bcrypt,db) {
     const {email,name,password}=req.body;
-    let state = true ;
+    let password = '';
+    // let state = true ;
     if(!email||!name||!password){
         return res.status(400).json('incorrect form subnission')
     }
     await bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(password, salt, function(err, hash) {
-             db('login')
-            .returning(['id','name','email'])
-            .insert({
-                email:email,
-                name:name,
-                password:hash,
-            }).then((user)=>{
-                return res.json(user[0]);
-            }).catch((err)=>{
-                state = false ;
-                console.log(err);
-            })
+            password=hash;
         });
     })
-    console.log(state);
-    if(!state){
+    db('login')
+    .returning(['id','name','email'])
+    .insert({
+        email:email,
+        name:name,
+        password:hash,
+    }).then((user)=>{
+        return res.json(user[0]);
+    }).catch((err)=>{
+        console.log(err);
         res.status(400).json("repeat email")
-    }
+    })
+    // console.log(state);
+    // if(!state){
+    //     res.status(400).json("repeat email")
+    // }
 }
 
 module.exports={
