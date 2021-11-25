99570da055b07b918dc67ce4967f1e0e6beb7e9a
YR1017fr <a27536625@gmail.com>
2021-11-25 15:39:37 +0800

    fix

--- controllders/register.js
+++ controllders/register.js
@@ -1,34 +1,24 @@
 const handleRegister = async function(req,res,bcrypt,db) {
     const {email,name,password}=req.body;
-    
-    // let state = true ;
     if(!email||!name||!password){
         return res.status(400).json('incorrect form subnission')
     }
-    let pass = bcrypt.genSalt(10, function(err, salt) {
-        let pass = bcrypt.hash(password, salt, function(err, hash) {
-            return hash;
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(password, salt, function(err, hash) {
+            db('login')
+            .returning(['id','name','email'])
+            .insert({
+                email:email,
+                name:name,
+                password:hash,
+            }).then((user)=>{
+                return res.json(user[0]);
+            }).catch((err)=>{
+                console.log(err);
+                res.status(400).json("repeat email");
+            })
         });
-        return pass ;
     })
-    console.log(pass,"密碼");
-    db('login')
-    .returning(['id','name','email'])
-    .insert({
-        email:email,
-        name:name,
-        password:pass,
-    }).then((user)=>{
-        console.log('work')
-        return res.json(user[0]);
-    }).catch((err)=>{
-        console.log(err);
-        res.status(400).json("repeat email")
-    })
-    // console.log(state);
-    // if(!state){
-    //     res.status(400).json("repeat email")
-    // }
 }
 
 module.exports={
