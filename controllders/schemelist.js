a16bdd33d6687832e0affe4273bfdafb0432b411
YR1017fr <a27536625@gmail.com>
2021-12-11 18:00:30 +0800

    test

---
+++ controllders/schemelist.js
@@ -0,0 +1,14 @@
+const getSchmemlist = (req, res, db) =>{
+    const {id} = req.params;
+    db.where({id}).select('schemename','no').from('scheme_base')
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+}
+
+module.exports = {
+    getSchmemlist : getSchmemlist
+}
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
-
 const register =require('./controllders/register');
 const signin =require('./controllders/signin');
 const scheme = require('./controllders/scheme');
-NODE_TLS_REJECT_UNAUTHORIZED='0'
-const db=knex({
+const schemelist = require('./controllders/schemelist');
+
+NODE_TLS_REJECT_UNAUTHORIZED = '0'
+const db = knex({
     client:'pg',
     connection: {
         connectionString : process.env.DATABASE_URL,
@@ -22,25 +23,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>res.json('it is working'))
-app.post('/signin',(req,res)=>signin.handleSignin(req,res,bcrypt,db))
-app.post('/register',(req,res)=>register.handleRegister(req,res,bcrypt,db))
-app.get('/schemelist/:id',(req,res)=>{//抓方案列表
-    const {id} = req.params;
-    db.where({
-        id:id
-    }).select('schemename','no').from('scheme_base')
-    .then(data=>{
-        res.json(data)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-})
-app.get('/scheme',(req,res)=>scheme.loadSchemeDetail(req,res,db))//載入方案
-app.post('/scheme/:id',(req,res)=>scheme.saveScheme(req,res,db))//儲存方案
-app.put('/scheme',(req,res)=>scheme.reviseScheme(req,res,db))//修改方案
-app.delete('/scheme',(req,res)=>scheme.deleteScheme(req,res,db))//修改方案
+app.get('/',(req,res) => res.json('it is working'))
+
+app.post('/signin',(req,res) => signin.handleSignin(req,res,bcrypt,db))//登入
+
+app.post('/register',(req,res) => register.handleRegister(req,res,bcrypt,db))
+
+app.get('/schemelist/:id',(req,res) => schemelist.getSchemelist(req,res,db))//抓方案列表
+
+app.get('/scheme',(req,res) => scheme.loadSchemeDetail(req,res,db))//載入方案
+
+app.post('/scheme/:id',(req,res) => scheme.saveScheme(req,res,db))//儲存方案
+
+app.put('/scheme',(req,res) => scheme.reviseScheme(req,res,db))//修改方案
+
+app.delete('/scheme',(req,res) => scheme.deleteScheme(req,res,db))//修改方案
    
 
 app.listen(process.env.PORT || 3001,()=>{   
