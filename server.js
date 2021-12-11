04a9dbc38ab0334af3b527be90b1e6fef2e43f37
YR1017fr <a27536625@gmail.com>
2021-12-11 18:04:51 +0800

    test

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/signin',(req,res) => signin.handleSignin(req,res,bcrypt,db))//登入
 
 app.post('/register',(req,res) => register.handleRegister(req,res,bcrypt,db))
 
-app.get('/schemelist/:id',(req,res) => schemelist.getSchemelist(req,res,db))//抓方案列表
+app.get('/schemelist/:id',(req,res) => schemelist.getSchmemlist(req,res,db))//抓方案列表
 
 app.get('/scheme',(req,res) => scheme.loadSchemeDetail(req,res,db))//載入方案
 
